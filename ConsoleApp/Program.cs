37ea32658b22daf56795f8562ad76beb973559a6
Baiguia <1942152252@qq.com>
2018-11-04 10:05:14 +0800

    DB frist实体

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -19,23 +19,37 @@ namespace ConsoleApp
                 Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
 
             //添加一条记录
-            var newDepartment = new Departments
-            {
-                ID = Guid.NewGuid(),
-                Name = "环境食品学院",
-                Dscn = "环境食品",
-                SortCode="007"
-            };
-            //把新的对象添加到上下文中
-            context.Departments.Add(newDepartment);
-            //更新上下文 把新的实体保存扫数据库中
+            //var newDepartment = new Departments
+            //{
+            //    ID = Guid.NewGuid(),
+            //    Name = "环境食品学院",
+            //    Dscn = "环境食品",
+            //    SortCode="007"
+            //};
+            ////把新的对象添加到上下文中
+            //context.Departments.Add(newDepartment);
+            ////更新上下文 把新的实体保存扫数据库中
+            //context.SaveChanges();
+
+            ////显示新的记录
+            //var departments1 = context.Departments.OrderBy(x => x.SortCode).ToList();
+            //foreach (var d in departments)
+            //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
+            Console.WriteLine("修改记录");
+            Console.WriteLine("===============================");
+            //修改
+            var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境食品学院");
+            editDepartment.Name = "环境食品";
+            editDepartment.SortCode = "007";
             context.SaveChanges();
 
-            //显示新的记录
+            else
+            Console.WriteLine("未找到该数据，不能修改");
+
             var departments1 = context.Departments.OrderBy(x => x.SortCode).ToList();
             foreach (var d in departments)
                 Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
-
             Console.ReadKey();
 
         }
