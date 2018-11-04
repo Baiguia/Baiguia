89882ca21faa7ec5448c15865e9a8ece59e9d078
Baiguia <1942152252@qq.com>
2018-11-04 10:35:10 +0800

    DB Frist

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,50 +10,54 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //创建数据的上下文，所有的表的实体都会在此上下文中
+            //使用数据上下文进行数据操作，using表示上下文代码的范围，执行完后内存会自动释放
+            using (var context = new CourseDBEntities())
+            {
+                var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments)
+                    Console.WriteLine("编号:{0},部门名称:{1},说明:{2}", d.SortCode, d.Name, d.Dscn);
 
-            //.where .orderby .tolist()注意调用的顺序
-            var context = new CourseDBEntities();
-            
-            var departments = context.Departments.OrderBy(x => x.SortCode).ToList();
-            foreach (var d in departments)
-                Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                //Console.WriteLine("修改记录");
+                //Console.WriteLine("========================================");
+                ////修改
+                //var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
+                //if (editDepartment != null)
+                //{
+                //    editDepartment.Name = "环境与食品检测学院";
+                //    editDepartment.SortCode = "007";
+                //    context.SaveChanges();
+                //}
+                //else
+                //    Console.WriteLine("未找到该记录，不能修改.");
 
-            //添加一条记录
-            //var newDepartment = new Departments
-            //{
-            //    ID = Guid.NewGuid(),
-            //    Name = "环境食品学院",
-            //    Dscn = "环境食品",
-            //    SortCode="007"
-            //};
-            ////把新的对象添加到上下文中
-            //context.Departments.Add(newDepartment);
-            ////更新上下文 把新的实体保存扫数据库中
-            //context.SaveChanges();
+                //Console.WriteLine("删除记录");
+                //Console.WriteLine("========================================");
+                ////find--用主键查询实体
+                //var delDept = context.Departments.Find(Guid.Parse("e6a729b7-7638-40b1-a8e7-ce9e0e666b70"));
+                ////var id = Guid.Parse("e6a729b7-7638-40b1-a8e7-ce9e0e666b70");
+                ////var delDept = context.Departments.SingleOrDefault(x => x.ID == id);
+                //context.Departments.Remove(delDept);
+                //context.SaveChanges();
 
-            ////显示新的记录
-            //var departments1 = context.Departments.OrderBy(x => x.SortCode).ToList();
-            //foreach (var d in departments)
-            //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                Console.WriteLine("添加记录");
+                Console.WriteLine("========================================");
+                var newDept = new Departments
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "测试学院",
+                    Dscn = "不可描述",
+                    SortCode = "999"
+                };
+                context.Departments.Add(newDept);
+                context.SaveChanges();
 
-            Console.WriteLine("修改记录");
-            Console.WriteLine("===============================");
-            //修改
-            var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境食品学院");
-            editDepartment.Name = "环境食品";
-            editDepartment.SortCode = "007";
-            context.SaveChanges();
-        
+                var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments1)
+                    Console.WriteLine("编号:{0},部门名称:{1},说明:{2}", d.SortCode, d.Name, d.Dscn);
 
-            else
-            Console.WriteLine("未找到该数据，不能修改");
-
-            var departments1 = context.Departments.OrderBy(x => x.SortCode).ToList();
-            foreach (var d in departments)
-                Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
-            Console.ReadKey();
-
+                Console.ReadKey();
+            }
         }
     }
 }
+
