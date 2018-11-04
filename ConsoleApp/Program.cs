49ebaaa2f1cee8e8c830bc9cf69cd4a634746999
Baiguia <1942152252@qq.com>
2018-11-04 08:29:49 +0800

    DB firse实体

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            //创建数据的上下文，所有的表的实体都会在此上下文中
+            var context = new CourseDBEntities();
+            var departments = context.Departments.OrderBy(x => x.SortCode).ToList();
+
+            foreach (var d in departments)
+                Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
+            Console.ReadKey();
         }
     }
 }
