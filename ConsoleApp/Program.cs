a141584141cc78d95ae79049cef29467080b86ca
Baiguia <1942152252@qq.com>
2018-11-04 08:41:09 +0800

    DB

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,8 +11,9 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             //创建数据的上下文，所有的表的实体都会在此上下文中
+            //.where .orderby .tolist()注意调用的顺序
             var context = new CourseDBEntities();
-            var departments = context.Departments.OrderBy(x => x.SortCode).ToList();
+            var departments = context.Departments.Where(x=>x.Name.Contains("艺术")).OrderBy(x => x.SortCode).ToList();
 
             foreach (var d in departments)
                 Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
