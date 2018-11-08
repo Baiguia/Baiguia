75b53c7ccd82ffc10fb40557dd4c9bb5872d0fe9
Baiguia <1942152252@qq.com>
2018-11-08 09:09:56 +0800

    11.5

--- CodeFirstDemo/CodeFirstDemo/Migrations/Configuration.cs
+++ CodeFirstDemo/CodeFirstDemo/Migrations/Configuration.cs
@@ -1,6 +1,5 @@
 namespace CodeFirstDemo.Migrations
 {
-    using CodeFirstModels;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -12,16 +11,13 @@ namespace CodeFirstDemo.Migrations
         {
             AutomaticMigrationsEnabled = true;
         }
-        //种子方法
+
         protected override void Seed(CodeFirstDemo.CodeFirstModels.CourseCotext context)
         {
-            //context.Database.ExecuteSqlCommand("delete courses");
-            //context.Database.ExecuteSqlCommand("delete departments");
-            //context.Database.ExecuteSqlCommand("delete students");
+            //  This method will be called after migrating to the latest version.
 
-            //DepartmentSeed.Seed(context);
-            //CourseCotext.Seed(context);
-            //StudentSeed.Seed(context);
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
         }
     }
 }
